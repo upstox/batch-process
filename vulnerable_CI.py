6d6d7a1046f048ccea71690cbf76432f5c42c78d
yogendrasrivastava <158015200+yogendrasrivastava@users.noreply.github.com>
2024-02-09 10:09:32 +0530

    Add files via upload
    This is the test file to validate the semgrep blocking, Doesn't contain real secret just a random aws creds.

---
+++ vulnerable_CI.py
@@ -0,0 +1,8 @@
+username = "AKIATTQJQRXT5A6C7PBA"
+password = "BA25/zGxlV3SQGTkjgajbfajkbfbfamdazWV0Qf/F/abcde"
+
+def run_command(command):
+    print(command)
+
+user_input = input("Enter a command to execute: ")
+run_command(user_input)
